51e74b1689066bb3937fe1d1b0d153bfcf680981
CTS\290692 <sagnik.das2@cognizant.com>
2018-03-08 11:11:17 +0530

    commt project file

--- App.js
+++ App.js
@@ -1,40 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-import home from './component/home/home';
-import about from './component/about/about';
-import contact from './component/contact/contact';
-
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/" exact>Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact US</Link>
-              </li>
-            </ul>
-          </nav>
-          <Route exact path="/" component={home} />
-          <Route path="/about" component={about} />
-          <Route path="/contact" component={contact} />
-        </div>
-      </Router>
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
     );
   }
 }
--- component/home/home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const home = () => {
-    return (
-        <h1>Home</h1>
-    );
-}
-
-export default home;
